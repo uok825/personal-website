0030592458870513dc80261c6f71418852b8537b
uok825 <omerleroyunda@gmail.com>
2024-07-03 10:28:45 +0300

    refactor: mobile updates

--- nextjs/components/Navbar.tsx
+++ nextjs/components/Navbar.tsx
@@ -2,41 +2,40 @@
 
 function Navbar() {
   return (
-    <header className="bg-black ">
-      <div className="flex text-white justify-between items-center ml-12 mr-12">
+    <header className="bg-black">
+      <div className="flex flex-col md:flex-row text-white justify-between items-center p-4 md:p-0 md:ml-12 md:mr-12">
         <a
           href="/"
-          className="hover:underline text-2xl font-bold font-montserrat"
+          className="hover:underline text-2xl font-bold font-montserrat mb-4 md:mb-0"
         >
-          {" "}
-          utkuomer.eth{" "}
+          utkuomer.eth
         </a>
-        <div className="flex items-end justify-end">
+        <div className="flex flex-col md:flex-row items-center md:items-end justify-end">
           <a
             href="/stack"
-            className="hover:underline font-montserrat font-thin p-3"
+            className="hover:underline font-montserrat font-thin p-2 md:p-3"
           >
             Stack
           </a>
           <a
             href="/experience"
-            className="hover:underline font-montserrat font-thin p-3"
+            className="hover:underline font-montserrat font-thin p-2 md:p-3"
           >
             Experience
           </a>
           <a
             href="/projects"
-            className="hover:underline font-montserrat font-thin p-3"
+            className="hover:underline font-montserrat font-thin p-2 md:p-3"
           >
             Projects
           </a>
           <a
             href="/contact"
-            className="hover:underline font-montserrat font-thin p-3"
+            className="hover:underline font-montserrat font-thin p-2 md:p-3"
           >
             Contact
           </a>
-          {/*           <Link href="https://github.com/uok825" target="_blank">
+          {/* <Link href="https://github.com/uok825" target="_blank">
             <Image
               src="github.svg"
               alt="GitHub"
--- nextjs/pages/projects.tsx
+++ nextjs/pages/projects.tsx
@@ -26,19 +26,22 @@ export default function Projects() {
   return (
     <div className="bg-black text-white min-h-screen min-w-full flex flex-col">
       <Navbar />
-      <h1 className="text-white font-bold font-poppins text-[70px] mb-32 text-center">
+      <h1 className="text-white font-bold font-poppins text-[40px] sm:text-[50px] md:text-[70px] mb-12 sm:mb-24 md:mb-32 text-center">
         Projects
       </h1>
 
-      <div className="relative flex items-center justify-center w-full mt-40 h-fit">
-        <button onClick={scrollLeft} className="z-10 size-16 -translate-x-8">
+      <div className="relative flex items-center justify-center w-full mt-20 sm:mt-40 h-fit">
+        <button
+          onClick={scrollLeft}
+          className="z-10 size-16 -translate-x-4 sm:-translate-x-8"
+        >
           <img src="arrow_back.png" alt="Scroll Left" />
         </button>
-        <div className="flex flex-row justify-center items-center space-x-6 overflow-x-hidden transition-transform duration-500">
+        <div className="flex flex-row justify-center items-center space-x-4 sm:space-x-6 overflow-x-hidden transition-transform duration-500">
           {visibleProjects.map((project, index) => (
             <div
               key={index}
-              className={`relative bg-white rounded-3xl shadow-lg w-[500px] h-[600px] p-4 transition-transform duration-500 transform ${getScaleClass(
+              className={`relative bg-white rounded-3xl shadow-lg w-[300px] sm:w-[400px] md:w-[500px] h-[400px] sm:h-[500px] md:h-[600px] p-4 transition-transform duration-500 transform ${getScaleClass(
                 index
               )}`}
             >
@@ -48,17 +51,17 @@ export default function Projects() {
                 alt=""
               />
 
-              <h2 className="text-3xl font-bold mb-4 mt-12 text-center text-gray-900">
+              <h2 className="text-xl sm:text-2xl md:text-3xl font-bold mb-2 sm:mb-4 mt-4 sm:mt-12 text-center text-gray-900">
                 {project.title}
               </h2>
-              <p className="text-gray-700 text-center leading-6 text-[18px] flex-grow pl-1 pr-1">
+              <p className="text-gray-700 text-center leading-6 text-sm sm:text-md md:text-lg flex-grow pl-1 pr-1">
                 {project.body}
               </p>
               <a
                 href={project.link}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="absolute bottom-8 left-1/2 p-[10px] text-center -translate-x-1/2 text-white font-bold bg-black border rounded-lg border-black w-[256px] h-[48px]"
+                className="absolute bottom-4 sm:bottom-8 left-1/2 p-[6px] sm:p-[10px] text-center -translate-x-1/2 text-white font-bold bg-black border rounded-lg border-black w-[200px] sm:w-[256px] h-[36px] sm:h-[48px]"
               >
                 {project.linkshortcut}
               </a>
@@ -67,7 +70,7 @@ export default function Projects() {
         </div>
         <button
           onClick={scrollRight}
-          className="z-10 size-16 translate-x-8 rotate-180"
+          className="z-10 size-16 translate-x-4 sm:translate-x-8 rotate-180"
         >
           <img src="arrow_back.png" alt="Scroll Right" />
         </button>
